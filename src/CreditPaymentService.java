21c663f277d0a9c6c72fdb076c5c1f4cb9ab6690
Neto test <aryamorozkova555@gmail.com>
2023-06-08 19:08:01 +0300

    Initial commit

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,7 +1,7 @@
 public class CreditPaymentService {
     public int calculate(int numberOfMonths, int amountOfCredit, double percent) {
-        double i = (percent / 12);
-        int k = (int) ((i * Math.pow((1 + i), numberOfMonths)) / (Math.pow((1 + i), numberOfMonths) - 1));
+        double i = (percent / 100 / 12);
+        double k = ((i * Math.pow((1 + i), numberOfMonths)) / (Math.pow((1 + i), numberOfMonths) - 1));
         int result = (int) (amountOfCredit * k);
         return result;
     }
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
 
-        int numberOfMonths = 12; //Количество месяцев
+        int numberOfMonths = 36; //Количество месяцев
         int amountOfCredit = 1_000_000;  //Сумма кредита
         double percent = 9.99;   //Процентная ставка
 
